ddd923534a3f6dc84edfd5c7cc0e0b9b84938932
Cayleigh <cayleighl@byui.edu>
2025-05-07 00:51:00 -0600

    updated grad invite page

---
+++ javascript/openEnvelope.js
@@ -0,0 +1,5 @@
+function openEnvelope() {
+    const envelope = document.getElementById("envelope");
+    // Toggle the "open" class to trigger the animation
+    envelope.classList.toggle("open");
+}
