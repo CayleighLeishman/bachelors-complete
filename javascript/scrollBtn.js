1a6c59b38bb9775737d5c8309bf34094aeb58d16
Cayleigh <cayleighl@byui.edu>
2025-10-26 13:34:46 -0600

    changed styles to use correct colors, added buttons and scroll to top accessibility

---
+++ javascript/scrollBtn.js
@@ -0,0 +1,12 @@
+// this script allows the "scroll to top" button to appear after scrolling
+const scrollBtn = document.getElementById("scrollTopBtn");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
